b63a9c38741568b17294bb75fc8fbee6ce27d4c4
AvinashYerra <101195675+AvinashYerra@users.noreply.github.com>
2025-09-14 00:56:50 +0530

    deployment fixes

--- checker.py
+++ checker.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -11,8 +12,14 @@ def run_accessibility_check(url):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
 
-    # Use Service object for webdriver
-    service = Service(ChromeDriverManager().install())
+    # Environment variable tells us if we're in Streamlit Cloud
+    if os.getenv("IS_STREAMLIT_CLOUD") == "true":
+        chrome_options.binary_location = "/usr/bin/chromium"
+        service = Service("/usr/bin/chromedriver")
+    else:
+        # Locally: Use Brave Browser binary path
+        chrome_options.binary_location = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+        service = Service(ChromeDriverManager().install())
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
